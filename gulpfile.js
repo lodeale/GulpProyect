214f501858fa56e2c86639958f0257bf22d4acc1
lodeale <lodeale@gmail.com>
2016-10-10 11:13:26 -0300

    Se agrega task para js y img

--- gulpfile.js
+++ gulpfile.js
@@ -33,14 +33,28 @@ gulp.task('pug', function(){
           .pipe(gulp.dest('.tmp'))
 });
 
+gulp.task('js', function() {
+  gulp.src('src/js/*.js')
+    .pipe($.plumber())
+    .pipe($.uglify({compress:true}))
+    .pipe(gulp.dest('.tmp'))
+});
+
+gulp.task('img', function(){
+  return gulp.src('src/img/*')
+          .pipe(gulp.dest('.tmp/img/'))
+});
+
 
 gulp.task('watch', function() {
   gulp.watch('src/css/*.sass', ['sass']);
+  gulp.watch('src/js/*', ['js']);
+  gulp.watch('src/img/*', ['img']);
   gulp.watch('src/*.pug', ['pug']);
   gulp.watch('.tmp/*.html').on('change', reload);
 });
 
-gulp.task('serve', ['sass','pug'], function() {
+gulp.task('serve', ['sass','pug', 'img'], function() {
   browserSync({
     server: {
       baseDir: ['.tmp','src']
@@ -77,11 +91,17 @@ gulp.task('pug:prod', function(){
           .pipe(gulp.dest('dist'));
 });
 
-gulp.task('js', function() {
+gulp.task('js:prod', function() {
   gulp.src('src/js/*.js')
     .pipe($.plumber())
     .pipe($.uglify({compress:true}))
     .pipe(gulp.dest('dist'))
 });
 
-gulp.task('build', ['sass:prod', 'pug:prod', 'js']);
+
+gulp.task('img:prod', function(){
+  return gulp.src('src/img/*')
+          .pipe(gulp.dest('dist/img/'))
+});
+
+gulp.task('build', ['sass:prod', 'pug:prod', 'js:prod','img:prod']);
