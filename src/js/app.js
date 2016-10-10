c131f76328028fac5e45a60cd5826e09c2ed31c1
lodeale <lodeale@gmail.com>
2016-10-10 16:43:14 -0300

    Se agrega un sitio como ejemplo

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,11 @@
 (function(){
-  var test = 'test';
-  console.log('test: ' , test);
+	$(function(){
+		$('#myCarousel').carousel();
+
+		var formInscription = $(".form-inscription");
+		var buttonInscription = $(".inscription");
+		buttonInscription.on('click', function(){
+			formInscription.css({display: 'block'}).animate({top:'300px'});
+		})
+	});
 })();
--- src/js/jquery.js
+++
@@ -1,3 +0,0 @@
-(function(){
-  console.log("Esto es jquery");
-})();
